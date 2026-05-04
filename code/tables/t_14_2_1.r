56d292f1f8d8b79220350c0743e162c2efda1c9a
Dmitry <dmitry.m.kolosov@gmail.com>
2026-05-03 23:30:05 -0300

    Final changes to 14.2.1

--- code/tables/t_14_2_1.r
+++ code/tables/t_14_2_1.r
@@ -18,6 +18,7 @@ library(gt)
 library(docorator)
 library(stringr)
 library(forcats)
+library(purrr)
 
 # Import utility functions
 source(file.path("code", "utils", "doc_relative_path.r"))
@@ -32,35 +33,74 @@ convert_blanks_to_na_local <- function(df) {
     mutate(across(where(is.character), ~ na_if(.x, "")))
 }
 
+# Convert all variable names to lowercase for code style consistency
 adsl <- read_dataset_json(file.path(path$adam, "adsl.json")) %>%
   convert_blanks_to_na_local() %>%
   rename_with(tolower) %>%
   filter(ittfl == "Y")
 
 # ----------------------------------------------------------------------------
+# Extra functions
+# ----------------------------------------------------------------------------
+
+format_percent <- function(x) {
+  # Apply your standard label_style_number formatting
+  default_fmt <- label_style_number(digits = 0, width = 3, align = "right", scale = 100)(x)
+
+  # Replace values > 0 and < 0.01 (1%) with " <1"
+  ifelse(!is.na(x) & x > 0 & x < 0.01,
+    " <1",
+    ifelse(!is.na(x) & (x >= 0.01 & x < 0.1),
+           paste0("\u2007", default_fmt),
+           default_fmt)
+  )
+}
+
+# ----------------------------------------------------------------------------
 # Prepare data
 # ----------------------------------------------------------------------------
 
-
-# BMI Category does not have a numeric vairable in ADSL, so order is defined manually
-adsl_factorized <- adsl %>%
+adsl_updated <- adsl %>%
+  # Factorize categorical variables to keep order in summaries
+  # BMI Category does not have a numeric vairable in ADSL, so order is defined manually
   mutate(
     trt01p = fct_reorder(factor(trt01p), trt01pn),
-    race = fct_reorder(factor(race), racen),
     agegr1 = fct_reorder(factor(agegr1), agegr1n),
     bmiblgr1 = factor(bmiblgr1, levels = c("<25", "25-<30", ">=30"))
+  ) %>%
+  # Update Sex values, as dataset values are abbreviated
+  mutate(
+    sex_updated = case_when(
+      sex == "M" ~ "Male",
+      sex == "F" ~ "Female",
+      TRUE ~ sex
+    ),
+    sex_updated = factor(sex_updated, levels = c("Male", "Female"))
+  ) %>%
+  # Add Race as collected in CRF
+  mutate(
+    race_crf = case_when(
+      ethnic == "HISPANIC OR LATINO" ~ "Hispanic",
+      race == "WHITE" ~ "Caucasian",
+      race == "BLACK OR AFRICAN AMERICAN" ~ "African Descent",
+      race == "AMERICAN INDIAN OR ALASKA NATIVE" ~ "Other",
+      TRUE ~ race
+    ),
+    race_crf = factor(race_crf, levels = c("Caucasian", "African Descent", "Hispanic", "Other"))
   )
 
 # ----------------------------------------------------------------------------
 # Get table summary data
 # ----------------------------------------------------------------------------
+
+# The variable list is reused in several places, so keep it as vector
 summary_cols <- c(
-  "age", "agegr1", "sex", "race", "mmsetot",
-  "durdis", "educlvl", "weightbl", "heightbl", "bmibl", "bmiblgr1"
+  "age", "agegr1", "sex_updated", "race_crf", "mmsetot",
+  "durdis", "durdsgr1", "educlvl", "weightbl", "heightbl", "bmibl", "bmiblgr1"
 )
 
 # Create summary table for demographic and baseline characteristics
-t_14_2_1 <- adsl_factorized %>%
+t_14_2_1 <- adsl_updated %>%
   tbl_summary(
     by = trt01p,
     type = all_continuous() ~ "continuous2",
@@ -69,28 +109,34 @@ t_14_2_1 <- adsl_factorized %>%
       all_continuous() ~ c("{N_nonmiss}", "{mean}", "{sd}", "{median}", "{min}", "{max}"),
       all_categorical() ~ "{n} ({p}%)"
     ),
+    # Do not show missing values
     missing = "no",
     digits = list(
       all_continuous() ~ c(0, 1, 2, 1, 1, 1),
       all_categorical() ~ list(
-        p = label_style_number(digits = 0, width = 3, justify = "right")
+        p = format_percent
       )
     ),
     label = list(
       age = "Age (y)",
-      agegr1 = "Age (y)",
-      sex = "Sex",
-      race = "Race (Origin)",
+      agegr1 = "Age Category",
+      sex_updated = "Sex",
+      race_crf = "Race (Origin)",
       mmsetot = "MMSE",
       durdis = "Duration of Disease",
+      durdsgr1 = "Duration of Disease Category",
       educlvl = "Years of Education",
       weightbl = "Baseline Weight (kg)",
       heightbl = "Baseline Height (cm)",
       bmibl = "Baseline BMI",
-      bmiblgr1 = "Baseline BMI"
+      bmiblgr1 = "Baseline BMI Category"
     )
   ) %>%
-  add_overall(last = TRUE) %>%
+  # Add Total column, label is assigned later for the stat_0 column
+  add_overall(
+    last = TRUE
+  ) %>%
+  # ANOVA for continuous (oneway.test + var.equal = TRUE) variables and chi-square test for categorical variables
   add_p(
     pvalue_fun = label_style_number(digits = 4),
     test = list(
@@ -101,24 +147,55 @@ t_14_2_1 <- adsl_factorized %>%
       all_continuous() ~ list(var.equal = TRUE)
     )
   ) %>%
+  # Manually set labels, as "n" default label is different
   add_stat_label(location = "row", label = list(
     all_continuous() ~ c("n", "Mean", "SD", "Median", "Min", "Max"),
     all_categorical() ~ ""
   )) %>%
+  # Column headers
   modify_header(label = "") %>%
   modify_header(list(
     all_stat_cols() ~ "**{level}**  \n(N={n})",
+    stat_0 ~ "**Total**  \n(N={N})",
     p.value ~ "**p-value  \n[1]**"
   )) %>%
   remove_footnote_header(columns = everything()) %>%
+  # Various formatting modifications
   modify_table_body(
     ~ .x %>%
+      # Add a blank line before each category
       mutate(variable = factor(variable, levels = summary_cols)) %>%
       group_by(variable, .add = TRUE) %>%
-      reframe(add_row(pick(everything()), .before = 1))
+      reframe(add_row(pick(everything()), .before = 1)) %>%
+      mutate(variable = as.character(variable)) %>%
+      # Reset stat label to remove extra comma for categorical variables
+      mutate(stat_label = NA) %>%
+      mutate(
+        across(
+          all_stat_cols(),
+          ~ case_when(
+            # Align values for continuous variables
+            (var_type == "continuous2" & row_type == "level" & label == "n") ~ paste0(.x, "\u2007\u2007\u2007"),
+            (var_type == "continuous2" & row_type == "level" & label != "SD") ~ paste0(.x, "\u2007"),
+            # Remove 0% in categorical rows
+            (var_type == "categorical" & row_type == "level" & . == "0 (  0%)")
+            ~ "0\u2007\u2007\u2007\u2007\u2007\u2007\u2007",
+            TRUE ~ .x
+          )
+        )
+      ) %>%
+      # To center the values, add 5 spaces at the end
+      mutate(
+        across(
+          all_stat_cols(),
+          ~ case_when(
+            # Align values for continuous variables
+            row_type == "level" ~ paste0(.x, "\u2007\u2007\u2007\u2007\u2007"),
+            TRUE ~ .x
+          )
+        )
+      )
   )
-
-tb <- t_14_2_1$table_body
 
 # ----------------------------------------------------------------------------
 # Output data
@@ -131,28 +208,47 @@ t_14_2_1_ard <- gather_ard(t_14_2_1)
 saveRDS(t_14_2_1_ard, file.path(path$table_ard, "t_14_2_1.rds"))
 
 # Save as PDF
-gt_table <-
+# We have to split table into several tables to avoid mid-page split of categories
+t_14_2_1_by_page <-
   t_14_2_1 %>%
-  as_gt() %>%
-  tab_style(
-    style = cell_text(align = "center"),
-    locations = cells_column_labels(columns = everything())
-  ) %>%
-  cols_width(
-    label ~ pct(30),
-    everything() ~ pct(13)
-  ) %>%
-  cols_align_decimal(columns = -label) %>%
-  tab_header(
-    title = "Table 14-2.01",
-    subtitle = "Summary of Demographic and Baseline Characteristics"
-  )
+  tbl_split_by_rows(variables = c(sex_updated, mmsetot, educlvl, heightbl))
+
+gt_tables_list <- map(t_14_2_1_by_page, ~ {
+  .x %>%
+    as_gt(auto_align = FALSE) %>%
+    tab_style(
+      style = cell_text(align = "center", v_align = "bottom"),
+      locations = cells_column_labels(columns = everything())
+    ) %>%
+    cols_align(
+      align = "right",
+      columns = all_stat_cols()
+    ) %>%
+    cols_align(
+      align = "center",
+      columns = p.value
+    ) %>%
+    cols_align(
+      align = "auto",
+      columns = label
+    ) %>%
+    cols_width(
+      label ~ "26%",
+      all_stat_cols() ~ "16%",
+      p.value ~ "10%"
+    ) %>%
+    tab_header(
+      title = "Table 14-2.01",
+      subtitle = "Summary of Demographic and Baseline Characteristics"
+    )
+})
 
 # Add header and footer using docorator
-gt_table %>%
+gt_group(.list = gt_tables_list) %>%
   as_docorator(
     display_name = "t_14_2_1",
     display_loc  = path$table_output,
+    tbl_scale = FALSE,
     header = fancyhead(
       fancyrow(left = "Protocol: CDISCPILOT01", center = NA, right = doc_pagenum()),
       fancyrow(left = "Population: Intent-to-Treat", center = NA, right = NA)
